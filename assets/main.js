c98e21d27ece015366332c9173735a6b662c21c8
Steven Martinez Pelaez <smartinezpelaez@gmail.com>
2024-05-20 20:57:10 -0500

    Updates

---
+++ assets/main.js
@@ -0,0 +1,43 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCBVjMGOIkavEAhyqpxJ73Dw&part=snippet%2Cid&order=date&maxResults=50';
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '15fe9962c1msh5adef8c8410037dp130dc2jsn113e5aa38382',
+		'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+  }
+
+  (async () => {
+    try {
+      const videos = await fetchData(API);
+      let view = `
+      ${videos.items.map(video => `
+        <div class="group relative">
+          <div
+            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          </div>
+          <div class="mt-4 flex justify-between">
+            <h3 class="text-sm text-gray-700">
+              <span aria-hidden="true" class="absolute inset-0"></span>
+              ${video.snippet.title}
+            </h3>
+          </div>
+        </div>
+      `).slice(0,4).join('')}
+  
+      `;
+      content.innerHTML = view;
+    } catch(err) {
+        console.log(err);
+    }
+  })();
